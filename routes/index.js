98eede99530b2a5583529a6ee970821c12249dcc
ysraelcon <ysraelcon@gmail.com>
2017-06-24 11:20:27 -0400

    1era pregunta, funcionando

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var quizController=require('../controllers/quiz24_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Bienvenido a Quiz---24' });
 });
+
+//ruta que lo manda al controller
+router.get("/quizes/question",quizController.question);
+router.get("/quizes/answer",quizController.answer);
 
 module.exports = router;
